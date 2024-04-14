3ff9fd1efbbbcedbe3829db9d1cc30c8307fc652
David Stevenson <drstevenson1958@yahoo.com>
2024-04-14 19:08:43 -0400

    Return Hello World from Home Controller Index method

--- ch02/FirstProject/Program.cs
+++ ch02/FirstProject/Program.cs
@@ -6,9 +6,8 @@ namespace FirstProject
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddRazorPages();
-
+            builder.Services.AddControllersWithViews();
+            
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
@@ -16,13 +15,23 @@ namespace FirstProject
             {
                 app.UseExceptionHandler("/Error");
             }
+
             app.UseStaticFiles();
 
             app.UseRouting();
 
-            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
 
-            app.MapRazorPages();
+            //app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            //app.MapRazorPages();
 
             app.Run();
         }
