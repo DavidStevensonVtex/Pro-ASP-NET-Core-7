08c79879785cad2022fefb5d9555044a30f0e547
David Stevenson <drstevenson1958@yahoo.com>
2024-04-15 17:21:30 -0400

    Listing 5.44 A lambda property in the Product.cs file in the Models folder

--- ch05/LanguageFeatures/Models/Product.cs
+++ ch05/LanguageFeatures/Models/Product.cs
@@ -5,6 +5,8 @@
         public string Name { get; set; } = string.Empty;
         public decimal? Price { get; set; }
 
+        public bool NameBeginsWithS => Name.Length > 0 && Name[0] == 'S';
+
         public static Product?[] GetProducts()
         {
             Product kayak = new Product { Name = "Kayak", Price = 275M };
