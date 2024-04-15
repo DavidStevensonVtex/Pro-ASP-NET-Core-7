ce49c7424c40cefbeb1d5ca17ad17de74ee82023
David Stevenson <drstevenson1958@yahoo.com>
2024-04-14 21:47:04 -0400

    Installing Bootstrap using libman

---
+++ ch04/InstallBootstrap.ps1
@@ -0,0 +1,3 @@
+Set-Location "D:\drs\ASPNETCore\Pro-ASP-NET-Core-7\ch04\MySolution\MyProject"
+libman install bootstrap@5.2.3 -d wwwrooot/lib/bootstrap --provider cdnjs
+Set-Location "D:\drs\ASPNETCore\Pro-ASP-NET-Core-7\ch04"
