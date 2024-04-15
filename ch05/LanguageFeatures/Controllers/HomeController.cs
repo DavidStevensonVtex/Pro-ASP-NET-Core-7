629af5f1201d6cb4d50963c8398157924d47bd24
David Stevenson <drstevenson1958@yahoo.com>
2024-04-15 18:37:32 -0400

    Listing 5.60 Hardcoding a name in the HomeController.cs file

--- ch05/LanguageFeatures/Controllers/HomeController.cs
+++ ch05/LanguageFeatures/Controllers/HomeController.cs
@@ -2,16 +2,17 @@
 {
     public class HomeController : Controller
     {
-        public async Task<ViewResult> Index()
+        public ViewResult Index()
         {
-            List<string> output = new List<string>();
-            await foreach (long? len in MyAsyncMethods.GetPageLengths(
-                output,
-                "manning.com", "microsoft.com", "amazon.com"))
+            var products = new[]
             {
-                output.Add($"Page length: {len:N0}");
-            }
-            return View(output);
+                new { Name = "Kayak", Price = 275M },
+                new { Name = "Lifejacket", Price = 48.95M },
+                new { Name = "Soccer ball", Price = 19.50M },
+                new { Name = "Corner flag", Price = 34.95M }
+            };
+
+            return View(products.Select(p => $"Name: {p.Name} Price: {p.Price:C2}"));
         }
     }
 }
