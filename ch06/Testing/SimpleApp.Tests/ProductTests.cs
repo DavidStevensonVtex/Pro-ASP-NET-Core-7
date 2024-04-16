0249f740258da2b8fadc84448c1106f6aa5562fc
David Stevenson <drstevenson1958@yahoo.com>
2024-04-16 13:22:58 -0400

    Listing 6.10 ProductTests.cs in the SimpleApp.Tests folder

---
+++ ch06/Testing/SimpleApp.Tests/ProductTests.cs
@@ -0,0 +1,31 @@
+namespace SimpleApp.Tests
+{
+    public class ProductTests
+    {
+        [Fact]
+        public void CanChangeProductName()
+        {
+            // Arrange
+            var p = new Product { Name = "Test", Price = 100M };
+
+            // Act
+            p.Name = "New Name";
+
+            // Assert
+            Assert.Equal("New Name", p.Name);
+        }
+
+        [Fact]
+        public void CanChangeProductPrice()
+        {
+            // Arrange
+            var p = new Product { Name = "Test", Price = 100M };
+
+            // Act
+            p.Price = 200M;
+
+            // Assert
+            Assert.Equal(100M, p.Price);
+        }
+    }
+}
