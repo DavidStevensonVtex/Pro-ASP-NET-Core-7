a9038467bbfb47039dc1278463e4985ba59b9f4c
David Stevenson <drstevenson1958@yahoo.com>
2024-04-16 14:01:03 -0400

    Listing 6.18 Isolating the controller in the HomeControllerTests.cs file in the SimpleApp.Tests folder

--- ch06/Testing/SimpleApp.Tests/HomeControllerTests.cs
+++ ch06/Testing/SimpleApp.Tests/HomeControllerTests.cs
@@ -4,11 +4,28 @@ namespace SimpleApp.Tests
 {
     public class HomeControllerTests
     {
+        class FakeDataSource : IDataSource
+        {
+            public IEnumerable<Product> Products { get; set; }
+            public FakeDataSource(Product[] data) => Products = data;
+        }
+
         [Fact]
         public void IndexActionModelIsComplete()
         {
             // Arrange
+            Product[] testData = new Product[]
+            {
+                new Product { Name = "P1", Price = 75.10M },
+                new Product { Name = "P2", Price = 120M },
+                new Product { Name = "P3", Price = 110M }
+            };
+
+            IDataSource data = new FakeDataSource(testData);
+
             var controller = new HomeController();
+            controller.dataSource = data;
+
             Product[] products = new Product[]
             {
                 new Product { Name = "Kayak", Price = 275M },
@@ -19,7 +36,7 @@ namespace SimpleApp.Tests
             var model = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
 
             // Assert
-            Assert.Equal(products, model, 
+            Assert.Equal(data.Products, model, 
                 Comparer.Get<Product>((p1, p2) => p1?.Name == p2?.Name && 
                     p1?.Price == p2?.Price));
         }
