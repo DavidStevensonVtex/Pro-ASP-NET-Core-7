f12803676410bd21d8d0aa50557dc6bb7ea52fdb
David Stevenson <drstevenson1958@yahoo.com>
2024-04-16 15:00:29 -0400

    Listing 7.5 Configuring the application in the Program.cs file in the SportsStore folder

--- ch07/SportsSln/SportsStore/Program.cs
+++ ch07/SportsSln/SportsStore/Program.cs
@@ -6,23 +6,12 @@ namespace SportsStore
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddRazorPages();
+            builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Error");
-            }
             app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapRazorPages();
+            app.MapDefaultControllerRoute();
 
             app.Run();
         }
