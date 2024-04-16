ec1f2606d41a10fb5430bd72e811861941c20522
David Stevenson <drstevenson1958@yahoo.com>
2024-04-16 15:50:53 -0400

    Listing 7.17 Configuring Entity Framework Core in the Program.cs file in the SportsStore folder

--- ch07/SportsSln/SportsStore/Program.cs
+++ ch07/SportsSln/SportsStore/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
+
 namespace SportsStore
 {
     public class Program
@@ -7,6 +10,9 @@ namespace SportsStore
             var builder = WebApplication.CreateBuilder(args);
 
             builder.Services.AddControllersWithViews();
+
+            builder.Services.AddDbContext<StoreDbContext>(opts =>
+                opts.UseSqlServer(builder.Configuration["ConnectionStrings:SportsStoreConnection"]));
 
             var app = builder.Build();
 
