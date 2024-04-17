69a4dc1d3544d3ce3c0a47268aba94c5ecb2faf4
David Stevenson <drstevenson1958@yahoo.com>
2024-04-17 11:52:13 -0400

    Listing 7.39 Adding classes to elements in the PageLinkTagHelper.cs file in the SportsStore/Infrastructure folder

--- ch07/SportsSln/SportsStore/Infrastructure/PageLinkTagHelper.cs
+++ ch07/SportsSln/SportsStore/Infrastructure/PageLinkTagHelper.cs
@@ -25,6 +25,11 @@ namespace SportsStore.Infrastructure
 
         public string? PageAction { get; set; }
 
+        public bool PageClassesEnabled { get; set; } = false;
+        public string PageClass { get; set; } = string.Empty;
+        public string PageClassNormal { get; set; } = string.Empty;
+        public string PageClassSelected { get; set; } = string.Empty;
+
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             if (ViewContext != null && PageModel != null)
@@ -35,6 +40,11 @@ namespace SportsStore.Infrastructure
                 {
                     TagBuilder tag = new TagBuilder("a");
                     tag.Attributes["href"] = urlHelper.Action(PageAction, new { productPage = i });
+                    if (PageClassesEnabled)
+                    {
+                        tag.AddCssClass(PageClass);
+                        tag.AddCssClass(i == PageModel.CurrentPage ? PageClassSelected : PageClassNormal);
+                    }
                     tag.InnerHtml.Append(i.ToString());
                     result.InnerHtml.AppendHtml(tag);
                 }
