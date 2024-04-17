18096fab11c275d0ea528397373716e0b3a75e61
David Stevenson <drstevenson1958@yahoo.com>
2024-04-17 11:12:37 -0400

    Listing 7.34 Adding a new route in the Program.cs file in the SportsStore folder

--- ch07/SportsSln/SportsStore/Program.cs
+++ ch07/SportsSln/SportsStore/Program.cs
@@ -21,6 +21,10 @@ namespace SportsStore
             var app = builder.Build();
 
             app.UseStaticFiles();
+
+            app.MapControllerRoute("pagination",
+                "Products/Page{productPage}",
+                new { controller = "Home", action = "Index" });
             app.MapDefaultControllerRoute();
 
             SeedData.EnsurePopulated(app);
