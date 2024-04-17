8171b13bcb1b2c9437bd3da0d44c7868731e4302
David Stevenson <drstevenson1958@yahoo.com>
2024-04-17 09:36:38 -0400

    Unit Testing: Testing repository access with HomeControllerTests.cs

--- ch07/SportsSln/SportsStore.Tests/GlobalUsings.cs
+++ ch07/SportsSln/SportsStore.Tests/GlobalUsings.cs
@@ -1,1 +1,3 @@
 global using Xunit;
+global using SportsStore.Controllers;
+global using SportsStore.Models;
---
+++ ch07/SportsSln/SportsStore.Tests/HomeControllerTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportsStore.Tests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void Can_Use_Repository()
+        {
+            // Arrange
+            Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[]
+            {
+                new Product { ProductID = 1, Name = "P1" },
+                new Product { ProductID = 2, Name = "P2" }
+            }).AsQueryable<Product>());
+
+            HomeController controller = new HomeController(mock.Object);
+
+            // Act
+            IEnumerable<Product>? result = (controller.Index() as ViewResult)?.ViewData.Model as IEnumerable<Product>;
+
+            // Assert
+            Product[] prodArray = result?.ToArray() ?? Array.Empty<Product>();
+            Assert.True(prodArray.Length == 2);
+            Assert.Equal("P1", prodArray[0].Name);
+            Assert.Equal("P2", prodArray[1].Name);
+        }
+    }
+}
