9f82390fa78f796448614aa46a162e202c086483
David Stevenson <drstevenson1958@yahoo.com>
2024-04-20 14:39:16 -0400

    Listing 8.9 Default.cshtml file in the SportsStore/Views/Shared/Components/NavigationMenu folder

--- ch08/SportsSln/SportsStore.Tests/NavigationMenuViewComponentTests.cs
+++ ch08/SportsSln/SportsStore.Tests/NavigationMenuViewComponentTests.cs
@@ -1,9 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc.ViewComponents;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SportsStore.Tests
 {
