33dd5e8ed9c4c9ec7508addb7a552f2edff80549
David Stevenson <drstevenson1958@yahoo.com>
2024-04-20 14:51:30 -0400

    Unit test: reporting the selected category

--- ch08/SportsSln/SportsStore.Tests/NavigationMenuViewComponentTests.cs
+++ ch08/SportsSln/SportsStore.Tests/NavigationMenuViewComponentTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc.ViewComponents;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
 
 namespace SportsStore.Tests
 {
@@ -30,6 +31,39 @@ namespace SportsStore.Tests
             Assert.True(Enumerable.SequenceEqual(
                 new string[] { "Apples", "Oranges", "Plums" }, 
                 results));
+        }
+
+        [Fact]
+        public void Indicates_Selected_Category()
+        {
+            string categoryToSelect = "Apples";
+            Mock<IStoreRepository> mock = new Mock<IStoreRepository>();
+            mock.Setup(m => m.Products).Returns((new Product[]
+            {
+                new Product { ProductID = 1, Name = "P1", Category = "Apples" },
+                new Product { ProductID = 4, Name = "P4", Category = "Oranges" },
+
+            }).AsQueryable<Product>());
+
+            NavigationMenuViewComponent target =
+                new NavigationMenuViewComponent(mock.Object);
+
+            target.ViewComponentContext = new ViewComponentContext
+            {
+                ViewContext = new ViewContext
+                {
+                    RouteData = new Microsoft.AspNetCore.Routing.RouteData()
+                }
+            };
+
+            target.RouteData.Values["category"] = categoryToSelect ;
+
+            // Act
+            string? result = (string?)(target.Invoke()
+                as ViewViewComponentResult)?.ViewData?["SelectedCategory"];
+
+            // Assert
+            Assert.Equal(categoryToSelect, result);
         }
     }
 }
