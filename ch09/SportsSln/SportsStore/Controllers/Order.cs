11a6ef5001826f3e2c914c7dafb8bf076a2aea5f
David Stevenson <drstevenson1958@yahoo.com>
2024-04-21 19:10:03 -0400

    Listing 9.13 The OrderController.cs file in the SportsStore/Controllers folder

---
+++ ch09/SportsSln/SportsStore/Controllers/Order.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SportsStore.Controllers
+{
+    public class Order : Controller
+    {
+        public IActionResult Checkout() => View(new Order());
+    }
+}
