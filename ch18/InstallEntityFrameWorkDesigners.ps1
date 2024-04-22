835de5e14602a20e0dc20af233eb5a299d7d33fe
David Stevenson <drstevenson1958@yahoo.com>
2024-04-22 12:39:15 -0400

    Listing 18.3 Adding packages to the project

---
+++ ch18/InstallEntityFrameWorkDesigners.ps1
@@ -0,0 +1,3 @@
+# Listing 18.3 Adding packages to the project
+dotnet add package Microsoft.EntityFrameworkCore.Design --version 7.0.18
+dotnet add package Microsoft.EntityFrameworkCore.SqlServer --version 7.0.18
