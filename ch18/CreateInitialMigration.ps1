bc56c66027d8e74bbcf366bbf937033557bc406e
David Stevenson <drstevenson1958@yahoo.com>
2024-04-22 13:31:32 -0400

    Listing 18.12 Creating an Entity Framework Core migration

---
+++ ch18/CreateInitialMigration.ps1
@@ -0,0 +1,2 @@
+# Listing 18.12 Creating an Entity Framework Core migration
+dotnet ef migrations add Initial
---
+++ ch18/UpdateDatabase.ps1
@@ -0,0 +1,2 @@
+# Listing 18.13 Applying the migration to the database
+dotnet ef database update
