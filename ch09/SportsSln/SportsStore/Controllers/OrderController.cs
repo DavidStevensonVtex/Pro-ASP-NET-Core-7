16d8e15b2239063916090c166498eeb3767f8a27
David Stevenson <drstevenson1958@yahoo.com>
2024-04-21 20:02:14 -0400

    Listing 9.20 Completing the OrderController.cs file in the SportsStore/Controllers folder
    Add the HTTP Post Checkout method.

--- ch09/SportsSln/SportsStore/Controllers/OrderController.cs
+++ ch09/SportsSln/SportsStore/Controllers/OrderController.cs
@@ -5,6 +5,35 @@ namespace SportsStore.Controllers
 {
     public class OrderController : Controller
     {
-        public IActionResult Checkout() => View(new Order());
+        private IOrderRepository repository;
+        private Cart cart;
+
+        public OrderController(IOrderRepository repoService)
+        {
+            repository = repoService;
+        }
+
+        public ViewResult Checkout() => View(new Order());
+
+        [HttpPost]
+        public IActionResult Checkout(Order order)
+        {
+            if (cart.Lines.Count() == 0)
+            {
+                ModelState.AddModelError("", "Sorry, your cart is empty!");
+            }
+
+            if (ModelState.IsValid)
+            {
+                order.Lines = cart.Lines.ToArray();
+                repository.SaveOrder(order);
+                cart.Clear();
+                return RedirectToPage("/Completed", new { orderId = order.OrderID });
+            }
+            else
+            {
+                return View();
+            }
+        }
     }
 }
