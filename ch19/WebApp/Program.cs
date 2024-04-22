fcc6a55944df9d93882a38d8fb2472a8239186da
David Stevenson <drstevenson1958@yahoo.com>
2024-04-22 14:20:39 -0400

    Listing 19.3 Creating a web service in the Program.cs file in the WebApp folder.

--- ch19/WebApp/Program.cs
+++ ch19/WebApp/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
 using WebApp.Models;
 
 namespace WebApp
@@ -17,7 +18,42 @@ namespace WebApp
 
             var app = builder.Build();
 
-            app.UseMiddleware<WebApp.TestMiddleware>();
+            const string BASEURL = "api/products";
+
+            app.MapGet($"{BASEURL}/{{id}}", async (HttpContext context, DataContext data) =>
+            {
+                string? id = context.Request.RouteValues["id"] as string;
+                if (id != null)
+                {
+                    Product? p = data.Products.Find(long.Parse(id));
+                    if (p == null)
+                    {
+                        context.Response.StatusCode = StatusCodes.Status404NotFound;
+                    }
+                    else
+                    {
+                        context.Response.ContentType = "application/json";
+                        await context.Response.WriteAsync(JsonSerializer.Serialize<Product>(p));
+                    }
+                }
+            });
+
+            app.MapGet(BASEURL, async (HttpContext context, DataContext data) =>
+            {
+                context.Response.ContentType = "application/json";
+                await context.Response.WriteAsync(JsonSerializer.Serialize<IEnumerable<Product>>(data.Products));
+            });
+
+            app.MapPost(BASEURL, async (HttpContext context, DataContext data) =>
+            {
+                Product? p = await JsonSerializer.DeserializeAsync<Product>(context.Request.Body);
+                if (p != null)
+                {
+                    await data.AddAsync(p);
+                    await data.SaveChangesAsync();
+                    context.Response.StatusCode = StatusCodes.Status200OK;
+                }
+            });
 
             app.MapGet("/", () => "Hello World!");
 
