8a87c58920c59ece0d5677a9267b3c329a1b8ed8
David Stevenson <drstevenson1958@yahoo.com>
2024-04-22 15:15:05 -0400

    Listing 19.10 Sending a POST request to add the Soccer Boots product

---
+++ ch19/CreateSoccerBootsProduct.ps1
@@ -0,0 +1,4 @@
+# Listing 19.10 Creating a Product with a POST request
+Invoke-RestMethod http://localhost:5000/api/products -Method POST `
+    -Body (@{ Name="Soccer Boots"; Price=89.99; CategoryId = 2; SupplierId = 2 } | `
+    ConvertTo-Json) -ContentType "application/json"
