16fde663ae677ecbd4614765febc4b31444331c4
David Stevenson <drstevenson1958@yahoo.com>
2024-04-22 14:36:09 -0400

    Listing 19.5 Enabling the MVC Fraemwork in the Program.cs file in the WebApp folder

--- ch19/WebApp/Program.cs
+++ ch19/WebApp/Program.cs
@@ -16,46 +16,11 @@ namespace WebApp
                 opts.EnableSensitiveDataLogging(true);
             });
 
+            builder.Services.AddControllers();
+
             var app = builder.Build();
 
-            const string BASEURL = "api/products";
-
-            app.MapGet($"{BASEURL}/{{id}}", async (HttpContext context, DataContext data) =>
-            {
-                string? id = context.Request.RouteValues["id"] as string;
-                if (id != null)
-                {
-                    Product? p = data.Products.Find(long.Parse(id));
-                    if (p == null)
-                    {
-                        context.Response.StatusCode = StatusCodes.Status404NotFound;
-                    }
-                    else
-                    {
-                        context.Response.ContentType = "application/json";
-                        await context.Response.WriteAsync(JsonSerializer.Serialize<Product>(p));
-                    }
-                }
-            });
-
-            app.MapGet(BASEURL, async (HttpContext context, DataContext data) =>
-            {
-                context.Response.ContentType = "application/json";
-                await context.Response.WriteAsync(JsonSerializer.Serialize<IEnumerable<Product>>(data.Products));
-            });
-
-            app.MapPost(BASEURL, async (HttpContext context, DataContext data) =>
-            {
-                Product? p = await JsonSerializer.DeserializeAsync<Product>(context.Request.Body);
-                if (p != null)
-                {
-                    await data.AddAsync(p);
-                    await data.SaveChangesAsync();
-                    context.Response.StatusCode = StatusCodes.Status200OK;
-                }
-            });
-
-            app.MapGet("/", () => "Hello World!");
+            app.MapControllers();
 
             var context = app.Services.CreateScope().ServiceProvider.GetRequiredService<DataContext>();
             SeedData.SeedDatabase(context);
