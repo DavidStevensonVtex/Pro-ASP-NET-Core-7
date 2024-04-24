1dc6960ede7ee90642a485f20b46dfa592d26473
David Stevenson <drstevenson1958@yahoo.com>
2024-04-24 19:50:55 -0400

    Listing 19.27 Using the [ApiController] attribute in the ProductsController.cs file in the Controllers folder

--- ch19/WebApp/Controllers/ProductsController.cs
+++ ch19/WebApp/Controllers/ProductsController.cs
@@ -3,6 +3,12 @@ using WebApp.Models;
 
 namespace WebApp.Controllers
 {
+    // Getting data from the body and validating data are required so commonly
+    // that they are applied automatically when the ApiController attribute is used
+    // requiring the focus of the code in the controller's action to 
+    // dealing with the applicatino features.
+    // page 521
+    [ApiController]
     [Route("api/[controller]")]
     public class ProductsController : ControllerBase
     {
@@ -31,20 +37,16 @@ namespace WebApp.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> SaveProduct([FromBody] ProductBindingTarget target)
+        public async Task<IActionResult> SaveProduct(ProductBindingTarget target)
         {
-            if (ModelState.IsValid)
-            {
-                Product p = target.ToProduct();
-                await context.Products.AddAsync(p);
-                await context.SaveChangesAsync();
-                return Ok(p);
-            }
-            return BadRequest(ModelState);
+            Product p = target.ToProduct();
+            await context.Products.AddAsync(p);
+            await context.SaveChangesAsync();
+            return Ok(p);
         }
 
         [HttpPut]
-        public async Task UpdateProduct([FromBody] Product product)
+        public async Task UpdateProduct(Product product)
         {
             context.Products.Update(product);
             await context.SaveChangesAsync();
