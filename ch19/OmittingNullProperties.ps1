6bf80053f52715e3eabe30a3d320c8bfc712a3ff
David Stevenson <drstevenson1958@yahoo.com>
2024-04-24 19:58:29 -0400

    Listing 19.28 Omitting Null property values from a GET request, results after ProductsController.cs change

--- ch19/OmittingNullProperties.ps1
+++ ch19/OmittingNullProperties.ps1
@@ -5,3 +5,8 @@ Invoke-WebRequest http://localhost:5000/api/products/1 | Select-Object Content
 # Content
 # -------
 # {"productId":1,"name":"Green Kayak","price":275.00,"categoryId":1,"category":null,"supplierId":1,"supplier":null}
+
+# After Code change
+# Content
+# -------
+# {"productId":1,"name":"Green Kayak","price":275.00,"categoryId":1,"supplierId":1}
