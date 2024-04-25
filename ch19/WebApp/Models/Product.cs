4500555d26c3c23c4eb3f48e8d50cbc4196db708
David Stevenson <drstevenson1958@yahoo.com>
2024-04-24 20:02:00 -0400

    Listing 19.30 Configuring the serializer in the Product.cs file in the Models folder

--- ch19/WebApp/Models/Product.cs
+++ ch19/WebApp/Models/Product.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace WebApp.Models
 {
@@ -12,9 +13,12 @@ namespace WebApp.Models
         public decimal Price { get; set; }
 
         public long CategoryId { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public Category? Category { get; set; }
 
         public long SupplierId { get; set; }
+
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         public Supplier? Supplier { get; set; }
     }
 }
