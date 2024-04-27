0c3eed8e6038b3a092b94b138e1d9c70609d36a5
David Stevenson <drstevenson1958@yahoo.com>
2024-04-27 13:42:02 -0400

    Chapter 20: Advanced web service features

---
+++ ch20/WebApp/Controllers/ProductsController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.RateLimiting;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    // Getting data from the body and validating data are required so commonly
+    // that they are applied automatically when the ApiController attribute is used
+    // requiring the focus of the code in the controller's action to 
+    // dealing with the applicatino features.
+    // page 521
+    [ApiController]
+    [Route("api/[controller]")]
+    [EnableRateLimiting("fixedWindow")]
+    public class ProductsController : ControllerBase
+    {
+        private DataContext context;
+
+        public ProductsController(DataContext ctx)
+        {
+            context = ctx;
+        }
+
+        [HttpGet]
+        public IAsyncEnumerable<Product> GetProducts()
+        {
+            return context.Products.AsAsyncEnumerable();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProduct(long id)
+        {
+            Product? p = await context.Products.FindAsync(id);
+            if (p == null)
+            {
+                return NotFound();
+            }
+            return Ok(p);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveProduct(ProductBindingTarget target)
+        {
+            Product p = target.ToProduct();
+            await context.Products.AddAsync(p);
+            await context.SaveChangesAsync();
+            return Ok(p);
+        }
+
+        [HttpPut]
+        public async Task UpdateProduct(Product product)
+        {
+            context.Products.Update(product);
+            await context.SaveChangesAsync();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task DeleteProduct(long id)
+        {
+            context.Products.Remove(new Product() { ProductId = id, Name = string.Empty });
+            await context.SaveChangesAsync();
+        }
+
+        [HttpGet("redirect")]
+        public IActionResult Redirect()
+        {
+            return RedirectToAction(nameof(GetProduct), new { Id = 1 });
+        }
+    }
+}
---
+++ ch20/WebApp/Program.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Http.Json;
+using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
+using WebApp.Models;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.RateLimiting;
+
+namespace WebApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<DataContext>(opts =>
+            {
+                opts.UseSqlServer(builder.Configuration["ConnectionStrings:ProductConnection"]);
+                opts.EnableSensitiveDataLogging(true);
+            });
+
+            builder.Services.AddControllers();
+
+            builder.Services.AddRateLimiter(opts =>
+                opts.AddFixedWindowLimiter("fixedWindow", fixOpts =>
+                {
+                    fixOpts.PermitLimit = 1;
+                    fixOpts.QueueLimit = 0;
+                    fixOpts.Window = TimeSpan.FromSeconds(15);
+                }));
+
+            builder.Services.Configure<JsonOptions>(opts =>
+                opts.SerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull);
+
+            // https://learn.microsoft.com/en-gb/aspnet/core/security/cors?view=aspnetcore-7.0
+            // The optoins pattern is used to configure CORS with the CorsOptions class defined
+            // in the Microsoft.AspNetCore.Cors.Infrastructure namespace.
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy(name: MyAllowSpecificOrigins,
+                    policy =>
+                    {
+                        policy.WithOrigins("http://example.com",
+                                            "http://www.contoso.com");
+                    });
+            });
+
+            var app = builder.Build();
+
+            app.UseRateLimiter();
+
+            app.MapControllers();
+
+            app.UseCors(MyAllowSpecificOrigins);
+
+            var context = app.Services.CreateScope().ServiceProvider.GetRequiredService<DataContext>();
+            SeedData.SeedDatabase(context);
+
+            app.Run();
+        }
+    }
+}
