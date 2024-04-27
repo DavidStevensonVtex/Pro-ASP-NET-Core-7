2b5a2890961f5301eba1e3d55cda84c133eb8cb1
David Stevenson <drstevenson1958@yahoo.com>
2024-04-27 16:22:22 -0400

    Listing 20.31 Declaring results in the ProductsController.cs file in the Controllers folder

--- ch20/WebApp/Controllers/ProductsController.cs
+++ ch20/WebApp/Controllers/ProductsController.cs
@@ -28,6 +28,8 @@ namespace WebApp.Controllers
         }
 
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<IActionResult> GetProduct(long id)
         {
             Product? p = await context.Products.FindAsync(id);
