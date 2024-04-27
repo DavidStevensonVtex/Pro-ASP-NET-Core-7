45a3e21d53e3245322b2faee6e1c3a7f49abf6eb
David Stevenson <drstevenson1958@yahoo.com>
2024-04-27 14:19:35 -0400

    Listing 20.8 Addign an action in the SuppleirsController.cs file in the Controllers folder

--- ch20/WebApp/Controllers/SuppliersController.cs
+++ ch20/WebApp/Controllers/SuppliersController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebApp.Models;
+using Microsoft.AspNetCore.JsonPatch;
 
 namespace WebApp.Controllers
 {
@@ -31,6 +32,18 @@ namespace WebApp.Controllers
             }
 
             return supplier;
+        }
+
+        [HttpPatch("{id}")]
+        public async Task<Supplier?> PatchSupplier(long id, JsonPatchDocument<Supplier> patchDoc)
+        {
+            Supplier? s = await context.Suppliers.FindAsync(id);
+            if (s != null)
+            {
+                patchDoc.ApplyTo(s);
+                await context.SaveChangesAsync();
+            }
+            return s;
         }
     }
 }
