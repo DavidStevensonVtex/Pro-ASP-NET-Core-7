5dd588d80522c1d4cd35628986f570bfcf97ac70
David Stevenson <drstevenson1958@yahoo.com>
2024-04-27 15:58:53 -0400

    Listing 20.27 Removing actions in the ContentController.cs file in the Controllers folder

--- ch20/WebApp/Controllers/ContentController.cs
+++ ch20/WebApp/Controllers/ContentController.cs
@@ -45,11 +45,11 @@ namespace WebApp.Controllers
             return $"JSON: {product.Name}";
         }
 
-        [HttpPost]
-        [Consumes("application/xml")]
-        public string SaveProductXml(ProductBindingTarget product)
-        {
-            return $"XML: {product.Name}";
-        }
+        //[HttpPost]
+        //[Consumes("application/xml")]
+        //public string SaveProductXml(ProductBindingTarget product)
+        //{
+        //    return $"XML: {product.Name}";
+        //}
     }
 }
