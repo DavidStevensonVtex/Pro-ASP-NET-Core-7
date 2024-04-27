50788d5a454a5a1fb3f7e1f2ac1952d087565c9d
David Stevenson <drstevenson1958@yahoo.com>
2024-04-27 14:32:07 -0400

    Listing 20.10 ContentController.cs in the Controllers folder

---
+++ ch20/WebApp/Controllers/ContentController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    [ApiController]
+    public class ContentController : ControllerBase
+    {
+        private DataContext context;
+
+        public ContentController(DataContext ctx)
+        {
+            context = ctx;
+        }
+
+        [HttpGet("string")]
+        public string GetString() => "This is a string response";
+
+        [HttpGet("object")]
+        public async Task<Product> GetObject()
+        {
+            return await context.Products.FirstAsync();
+        }
+    }
+}
