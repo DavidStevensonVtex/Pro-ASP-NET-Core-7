909c6ebf03c9d588845b53cd399e2ee12247ab08
David Stevenson <drstevenson1958@yahoo.com>
2024-04-27 15:11:49 -0400

    Listing 20.17 Requesting a PNG response

---
+++ ch20/Request_a_PNG_Response.ps1
@@ -0,0 +1,11 @@
+# Listing 20.17 Requesting a PNG response
+Invoke-WebRequest http://localhost:5000/api/content/object `
+    -Headers @{Accept="img/png" } | `
+    Select-Object @{n='Content Type'; e={ $_.Headers."Content-Type" }}, Content
+
+
+# The image request Accept means that a JSON response will be sent
+
+# Content Type                    Content
+# ------------                    -------
+# application/json; charset=utf-8 {"name":"Kayak","price":275.00,"categoryId":1,"supplierId":1}
