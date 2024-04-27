77639651d32805ea028975688c96ce91f510b53a
David Stevenson <drstevenson1958@yahoo.com>
2024-04-27 15:17:40 -0400

    Rerun Listing 20.17 After setting RespectBrowserAcceptHeader to true in Program.cs file

--- ch20/Request_a_PNG_Response.ps1
+++ ch20/Request_a_PNG_Response.ps1
@@ -9,3 +9,7 @@ Invoke-WebRequest http://localhost:5000/api/content/object `
 # Content Type                    Content
 # ------------                    -------
 # application/json; charset=utf-8 {"name":"Kayak","price":275.00,"categoryId":1,"supplierId":1}
+
+# After setting RespectBrowserAcceptHeader to true in Program.cs file:
+
+#      | Response status code does not indicate success: 406 (Not Acceptable).
